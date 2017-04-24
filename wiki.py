3eb272ebe5e7593892731d2ec1ac0905383c3eb8
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2017-04-24 10:30:09 +0200

    Implement sparql lookup at wikidata.

--- wiki.py
+++ wiki.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python
 
+import re
 import pywikibot as wiki
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+sparqli = SPARQLWrapper('https://query.wikidata.org/bigdata/namespace/wdq/sparql')
+sparqli.setReturnFormat(JSON)
 
 site = wiki.Site('wikidata', 'wikidata')
 repo = site.data_repository()
 
+def extract_id(url):
+  """ Extracts item or property ID from any string."""
+  return re.findall("[PpQq][1-9][0-9]*", url)[0]
+
 def item(id):
+  if not id.lower().startswith('q'):
+    id = extract_id(id)
   return wiki.ItemPage(repo, id)
 
 
 def prop(id):
+  if not id.lower().startswith('p'):
+    id = extract_id(id)
   return wiki.PropertyPage(repo, id)
 
 
@@ -20,4 +34,9 @@ def label(entity):
   else:
     for v in labels.values():
       return v
+
+def sparql(query, limit=500):
+  sparqli.setQuery('select * where {{{}}} limit {}'.format(query, limit))
+  results = sparqli.query().convert()
+  return results.get('results', {}).get('bindings', [])
 
